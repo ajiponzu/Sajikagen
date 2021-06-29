e6a0b0d8e6354542f7dba4bfef57513972ed71ef
ajiponzu <cpjvmm@outlook.jp>
2021-06-29 20:56:36 +0900

    結合

--- popup.js
+++ popup.js
@@ -1,10 +1,12 @@
-/*chrome.storage.onChanged.addListener(function (changes, namespace){
-    for(let [key, {oldValue, newValue}] of Object.entries(changes)){
-        console.log(`key is ${key}, namespace is ${namespace}, oldValue is ${oldValue}, newValue is ${newValue}`)
-    }
-})*/
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    // for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+    //     console.log(`key is ${key}, namespace is ${namespace}, oldValue is ${oldValue}, newValue is ${newValue}`)
+    // }
+    iframe2.location.href = `https://ja.wikipedia.org/wiki/${result.key}`;
+    location.reload();
+})
 
-chrome.storage.local.get(['key'], function(result) {
-     iframe2.location.href = `https://ja.wikipedia.org/wiki/${result.key}`
-    console.log('Value currently is ' + result.key)
-  });
+chrome.storage.local.get(['key'], function (result) {
+    iframe2.location.href = `https://ja.wikipedia.org/wiki/${result.key}`;
+    console.log('Value currently is ' + result.key);
+});

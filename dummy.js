53c27c967fa8441600739bde5371710e0e57244d
ajiponzu <cpjvmm@outlook.jp>
2021-06-29 19:57:26 +0900

    ダミー削除

--- dummy.js
+++
@@ -1,7 +0,0 @@
-chrome.storage.onChanged.addListener(function (changes, namespace) {
-  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-    console.log(
-      `${key}, ${site}, ${namespace}, ${oldValue}, ${newValue}`
-    );
-  }
-});

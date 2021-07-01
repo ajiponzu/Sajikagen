9940a6990c3fe00fbe88516b6ea39e22b39a9269
onikun94 <onikun94@gmail.com>
2021-07-01 18:15:32 +0900

    機能実装完了（仮）

--- popup.js
+++ popup.js
@@ -1,12 +1,21 @@
+console.log("popup called")
 chrome.storage.onChanged.addListener(function (changes, namespace) {
-    // for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-    //     console.log(`key is ${key}, namespace is ${namespace}, oldValue is ${oldValue}, newValue is ${newValue}`)
-    // }
-    iframe2.location.href = `https://ja.wikipedia.org/wiki/${result.key}`;
-    location.reload();
+  // for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+  //     console.log(`key is ${key}, namespace is ${namespace}, oldValue is ${oldValue}, newValue is ${newValue}`)
+  // }
+  console.log("hoge")
+
+  location.reload()
 })
 
-chrome.storage.local.get(['key'], function (result) {
-    iframe2.location.href = `https://ja.wikipedia.org/wiki/${result.key}`;
-    console.log('Value currently is ' + result.key);
-});
+//初回のスクリプト読み込み時には意味がない(今音声認識されたものは取得されない)
+//上のstorage.onChanged.が呼ばれた段階でpopup.jsが再読み込みされるので期待されたタイミングで音声認識結果を得ることができる
+chrome.storage.local.get(["key"], function (result) {
+  if (result.key) {
+     searchRes.location.href = `https://ja.wikipedia.org/wiki/${result.key}`
+    //searchRes.location.href = `https://google.com/search?q=${result.key}`
+    //location.href = `https://google.com/search?q=${result.key}`
+    console.log("popup Value currently is " + result.key)
+  }
+})
+
--- recognize.js
+++ recognize.js
@@ -1,35 +1,31 @@
 /* 音声認識, 参考「https://jellyware.jp/kurage/iot/webspeechapi.html」 */
 const startApplication = () => {
-  window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
-
-  let reco = new webkitSpeechRecognition();
-
-  reco.lang = 'ja-JP';
-  reco.continuous = true;
-
+  window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition
+  console.log("recognize called")
+  let reco = new webkitSpeechRecognition()
+  reco.lang = "ja-JP"
+  reco.continuous = true
   reco.onerr = (event) => {
-    console.log(event.error);
-  };
-
+    console.log(event.error)
+  }
   reco.onsoundend = () => {
-    startApplication();
-  };
-
+    console.log("onsoundend")
+    startApplication()
+  }
   reco.onresult = (e) => {
-    let result = "";
-    [...e.results].slice(e.resultIndex).forEach((results) => {
-      result = results[0].transcript;
-    });
-    chrome.storage.local.set(
-      { key: result },
-      () => {
-        console.log('set: ' + result);
-      }
-    );
-    startApplication();
-  };
-
-  reco.start();
-};
-
-startApplication();
+    let result = ""
+    ;[...e.results].slice(e.resultIndex).forEach((results) => {
+      result = results[0].transcript
+    })
+    const res = confirm("検索しますか？")
+    if(res){
+      chrome.storage.local.set({ key: result }, () => {
+        console.log("set: " + result)
+      })
+    }
+    
+    startApplication()
+  }
+  reco.start()
+}
+startApplication()
